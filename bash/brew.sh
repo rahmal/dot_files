86a7ef895314e539b036b7fb329837bf3e1745ad
Rahmal Conda <rahmal@gmail.com>
2023-09-26 17:39:32 -0700

    Updated brew completions.

---
+++ bash/brew.sh
@@ -0,0 +1,15 @@
+# Source Homebrew's completion definitions to make them available in bash.
+
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
