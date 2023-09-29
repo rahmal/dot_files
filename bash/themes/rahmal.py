b49a6d291778e83a14915c0a3c110df2b1fb4f01
Rahmal Conda <rahmal@gmail.com>
2023-09-28 21:47:26 -0700

    Customized formatting for prompt.

--- bash/themes/rahmal.py
+++ bash/themes/rahmal.py
@@ -3,8 +3,11 @@ from powerline_shell.themes.default import DefaultColor
 
 class Color(DefaultColor):
     """Basic theme which only uses colors in 0-15 range"""
-    USERNAME_FG = 250
-    USERNAME_BG = 240
+    TIME_FG = 14#254
+    TIME_BG = 0 #166 #31
+
+    USERNAME_FG = 0 #250
+    USERNAME_BG = 14 #161
     USERNAME_ROOT_BG = 124
 
     HOSTNAME_FG = 250
@@ -13,8 +16,8 @@ class Color(DefaultColor):
     HOME_SPECIAL_DISPLAY = True
     HOME_BG = 31  # blueish
     HOME_FG = 15  # white
-    PATH_BG = 237  # dark grey
-    PATH_FG = 250  # light grey
+    PATH_BG = 4 #237  # dark grey
+    PATH_FG = 15 #250  # light grey
     CWD_FG = 254  # nearly-white grey
     SEPARATOR_FG = 244
 
@@ -25,23 +28,3 @@ class Color(DefaultColor):
     REPO_CLEAN_FG = 0   # black
     REPO_DIRTY_BG = 1   # red
     REPO_DIRTY_FG = 15  # white
-
-    JOBS_FG = 14
-    JOBS_BG = 8
-
-    CMD_PASSED_BG = 8
-    CMD_PASSED_FG = 15
-    CMD_FAILED_BG = 11
-    CMD_FAILED_FG = 0
-
-    SVN_CHANGES_BG = REPO_DIRTY_BG
-    SVN_CHANGES_FG = REPO_DIRTY_FG
-
-    VIRTUAL_ENV_BG = 2
-    VIRTUAL_ENV_FG = 0
-
-    AWS_PROFILE_FG = 14
-    AWS_PROFILE_BG = 8
-
-    TIME_FG = 8
-    TIME_BG = 7
